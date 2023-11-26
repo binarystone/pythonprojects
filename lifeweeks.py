8eed7e968abc863d52b15195fe6ffe73a22c0573
Pedro <psinger9000@gmail.com>
2023-11-26 23:43:50 +0800

    updated lifeweeks.py

--- lifeweeks.py
+++ lifeweeks.py
@@ -1,9 +1,11 @@
-age = input("How old are you?")
-life_expectancy = input("What is your expected life expectancy? ")
+age = int(input("How old are you? "))
+life_expectancy = int(input("What is your expected life expectancy? "))
 
 lifeweeks = age * 52
+remainingweeks = (life_expectancy - age) * 52
 
-print ("You have already lived " + )
+print (f"You have already lived {lifeweeks} weeks")
 
-print ("You have " + str(lifeweeks) + " weeks left")
-lifeexpectancyweeks = int(life_expectancy) * 52
+print ("You have " + str(remainingweeks) + " weeks left")
+
+print ("Live life to the fullest :)")
