ad3b858eecb9e8127d195b6022840269752bc7d3
Pedro <psinger9000@gmail.com>
2023-11-28 07:00:12 +0800

    added rideconditional.py

---
+++ rideconditional.py
@@ -0,0 +1,10 @@
+# calculate ride age
+
+age = int(input("How old are you? "))
+
+if age < 12:
+    print("Hmm you are too young")
+elif age < 18:
+    print("Hmm you are a cutie")
+else:
+    print("alright next")
