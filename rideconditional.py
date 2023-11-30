41d2621fd6fcc9261483c33c83ce7054cef38a26
binarystone <87140383+binarystone@users.noreply.github.com>
2023-11-30 22:09:40 +0800

    Update rideconditional.py

--- rideconditional.py
+++ rideconditional.py
@@ -3,8 +3,8 @@
 age = int(input("How old are you? "))
 
 if age < 12:
-    print("Hmm you are too young")
+    print("Hmm you are too young, this ride ain't for youngsters!")
 elif age < 18:
     print("Hmm you are a just right")
 else:
-    print("alright next")
+    print("alright next, this ride ain't for oldies!")
